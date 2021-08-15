60773589f949052b84f0e3f0a8b79660a3a1fff9
CyberLando42 <88796033+CyberLando42@users.noreply.github.com>
2021-08-15 14:25:03 -0500

    Create cookie_encde.py

---
+++ cookie_encde.py
@@ -0,0 +1,25 @@
+from flask import Flask, url_for, render_template, abort, send_file, request, redirect, session, make_response
+inputed_key = input("Flask session key:")
+inputed_ckey= input("Cookie key: ")
+inputed_val = input("Cookie value: ")
+print("Decoding...")
+app = Flask(__name__)
+app.secret_key = inputed_key
+
+@app.route('/')
+def index():
+    resp = make_response(redirect(url_for("test")))
+    session[inputed_ckey] = inputed_val
+    
+    return resp
+    
+    
+@app.route('/test')
+def test():
+
+    resp = make_response(render_template("test.html",  sessionc=request.cookies.get('session')))
+    
+    return resp
+    
+
+app.run(port=8080)
